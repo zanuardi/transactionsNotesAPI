fb62bdd9d34a6e796bd23c80ed5f1c7f9b081d48
Zanuardi Novanda <zanuardinovanda@gmail.com>
2020-10-22 13:47:02 +0700

    fix bug bearer token

--- middleware/authorizationMiddleware.js
+++ middleware/authorizationMiddleware.js
@@ -3,11 +3,14 @@ const hyperId = require('hyperid')
 function authorization(req, res, next) {
 
     const authorization = req.headers.authorization
-    const token = authorization.split(' ')[1]
-    const isValidToken = hyperId.decode(token)
-
-    if (isValidToken) {
-        next()
+    if (authorization) {
+        const token = authorization.split(' ')[1]
+        const isValidToken = hyperId.decode(token)
+        if (isValidToken) {
+            next()
+        } else {
+            res.status(401).send('Unauthorized')
+        }
     } else {
         res.status(401).send('Unauthorized')
     }
